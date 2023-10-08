3c3de15488f6c232ba26d319c9f5fcd4fee3f54d
Caio <caiojulian@hotmail.com>
2023-10-08 13:09:23 -0300

    refactor: update product test

--- src/usecase/product/update/update.product.integration.spec.ts
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -1,11 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
-import ProductFactory from "../../../domain/product/factory/product.factory";
 import UpdateProductUseCase from "./update.product.usecase";
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
 import Product from "../../../domain/product/entity/product";
 
-describe("Unit test for product update use case", () => {
+describe("Integration test for product update use case", () => {
   let sequelize: Sequelize;
 
   beforeEach(async () => {
