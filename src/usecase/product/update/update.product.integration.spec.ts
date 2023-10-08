7f69c0b358a5c1376e619cbb23e28f5cdec3ecc6
Caio <caiojulian@hotmail.com>
2023-10-08 12:53:02 -0300

    test: usecase update product integration

---
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,45 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import UpdateProductUseCase from "./update.product.usecase";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import Product from "../../../domain/product/entity/product";
+
+describe("Unit test for product update use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("a", "Product Alfa", 123);
+
+    await productRepository.create(product);
+
+    const input = {
+      id: product.id,
+      name: "Product Beta",
+      price: 789,
+    };
+
+    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+
+    const output = await productUpdateUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
